80afaab5e59e34f01a789a294aea0fcd06e38658
micty <mictydeng@qq.com>
2021-09-01 15:12:31 +0800

    2.0.1

--- modules/Path.js
+++ modules/Path.js
@@ -6,25 +6,24 @@ const path = require('path');
 * 路工具。
 */
 module.exports = exports = {
-
-    /**
-    * 原始的内置的 path 模块，为了方便引用。
-    */
-    path,
     
     /**
-    * 对路径进行标准化处理，以得到格式统一的路径。
+    * 对路径进行标准化处理，以得到不同操作系统下的格式统一的路径。
+    * @param {string} p 输入的路径字符串，可以是一个文件，也可以是一个目录。
+    * @returns {string} 返回格式统一的路径，即把 '\' 换成 '/'，把多个 '/' 合成一个。
     */
-    normalize(file) {
-        file = path.normalize(file);
-        file = file.replace(/\\/g, '/');    //把 '\' 换成 '/'。
-        file = file.replace(/\/+/g, '/');   //把多个 '/' 合成一个。
-
-        return file;
+    normalize(p) {
+        p = path.normalize(p);
+        p = p.replace(/\\/g, '/');    //把 '\' 换成 '/'。
+        p = p.replace(/\/+/g, '/');   //把多个 '/' 合成一个。
+        
+        return p;
     },
 
     /**
-    * 对目录路径进行标准化处理，以得到格式统一的路径，并且确保是以 `/` 结尾。
+    * 对目录路径进行标准化处理，以得到格式统一的目录路径。
+    * @param {string} dir 要进行处理的目录路径。 经过标准化处理后，如果不是以 `/` 结尾，则自动加上。
+    * @returns {string} 返回一个以 `/` 结尾的字符串。
     */
     normalizeDir(dir) {
         dir = exports.normalize(dir);
@@ -39,9 +38,21 @@ module.exports = exports = {
 
     /**
     * 获取指定路径的所在目录。
+    * @param {string} p 要处理的路径。
+    *   如果输入的是一个目录形式的字符串，则标准化后直接返回。
+    *   如果输入的是一个文件形式的字符串，则返回文件所在的目录。
+    * @returns {string} 返回以 `/` 结尾的字符串作为目录。
     */
-    dirname(file) {
-        let dir = path.dirname(file);
+    dirname(p) {
+        p = exports.normalize(p);
+
+        //已经是一个目录。
+        if (p.endsWith('/')) {
+            return p;
+        }
+
+        //是一个文件。
+        let dir = path.dirname(p);
         return exports.normalizeDir(dir);
     },
 
@@ -49,32 +60,32 @@ module.exports = exports = {
     * 合并多个路径，并格式化成标准形式，即以 `/` 作为目录的分隔符。
     */
     join(...args) {
-        let file = path.join(...args);
-        return exports.normalize(file);
+        let p = path.join(...args);
+        return exports.normalize(p);
     },
 
     /**
     * 获取相对路径。
     */
     relative(...args) {
-        let file = path.relative(...args);
-        return exports.normalize(file);
+        let p = path.relative(...args);
+        return exports.normalize(p);
     },
 
     /**
     * 获取相对路径。
     */
     resolve(...args) {
-        let file = path.resolve(...args);
-        return exports.normalize(file);
+        let p = path.resolve(...args);
+        return exports.normalize(p);
     },
 
     /**
     * 从对象返回路径字符串。
     */
     format(...args) {
-        let file = path.format(...args);
-        return exports.normalize(file);
+        let p = path.format(...args);
+        return exports.normalize(p);
     },
 
     
---
+++ test.js
@@ -0,0 +1,10 @@
+
+
+const Path = require('./modules/Path');
+
+console.log(`/a\\b/c\\d`, '-->', Path.normalize(`/a\\b/c\\d`))
+console.log(`/a\\b/c\\d`, '-->', Path.normalizeDir(`/a\\b/c\\d`))
+
+console.log('/a/b/c/', '-->', Path.dirname('/a/b/c/'));
+console.log('/a/b/c\\', '-->', Path.dirname('/a/b/c\\'));
+console.log('/a/b/c', '-->',Path.dirname('/a/b/c'));
