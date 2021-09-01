77d217e31913db7580dcdda109801d22922d0f86
micty <mictydeng@qq.com>
2021-09-01 17:02:44 +0800

    test.js

--- test.js
+++ test.js
@@ -7,4 +7,9 @@ console.log(`/a\\b/c\\d`, '-->', Path.normalizeDir(`/a\\b/c\\d`))
 
 console.log('/a/b/c/', '-->', Path.dirname('/a/b/c/'));
 console.log('/a/b/c\\', '-->', Path.dirname('/a/b/c\\'));
-console.log('/a/b/c', '-->',Path.dirname('/a/b/c'));
+console.log('/a/b/c', '-->', Path.dirname('/a/b/c'));
+console.log(Path.join('/a/b', 'c'));
+console.log(Path.join('/a/b/', 'c'));
+console.log(Path.join('/a/b/', '/c'));
+console.log(Path.join('/a/b/', '/c/'));
+console.log(Path.join('/a/b', '/c\\'));
